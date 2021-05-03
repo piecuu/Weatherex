549be74f74fb4c43f7908d114416ea72d1319d42
Szymon Piecek <onimusht@gmail.com>
2021-05-03 14:44:17 +0200

    Added basic controller and service for getting weather

---
+++ backend/src/Web/Weatherex.Api/Controllers/WeatherController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weatherex.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class WeatherController : ControllerBase
+    {
+        public WeatherController()
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetWeatherInTimeAsync(string city)
+        {
+
+        }
+    }
+}
