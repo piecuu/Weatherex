cc9ae48c048c7f95f498ce9b0b80fe91682d4a3b
Szymon Piecek <onimusht@gmail.com>
2021-05-03 14:52:23 +0200

    Basic model for weather

---
+++ backend/src/Core/Weatherex.Domain/Entities/Weather.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Weatherex.Domain.Entities
+{
+    public class Weather
+    {
+        public string Title { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public string WeatherState { get; set; }
+
+        public string WeatherAbbreviation { get; set; }
+    }
+}
